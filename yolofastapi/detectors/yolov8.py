cd4beab91baa8b54a8da8c50ee19a2424e25cc5c
Alexander Foley <alexandergfoley@gmail.com>
2023-10-31 11:46:16 -0600

    adding docstrings

--- yolofastapi/detectors/yolov8.py
+++ yolofastapi/detectors/yolov8.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 import cv2
-from urllib.request import urlopen
 import os
 import platform
 from ultralytics import YOLO
@@ -9,27 +8,26 @@ from ultralytics import YOLO
 
 
 class YoloV8ImageObjectDetection:
-    """
-    Class implements Yolo8 model to make inferences on a youtube video using Opencv2.
-    """
-    
-    PATH        = os.environ.get("YOLO_WEIGHTS_PATH", "yolov8n.pt")
-    CONF_THRESH = float(os.environ.get("YOLO_CONF_THRESHOLD", "0.10"))
-    IOU         = float(os.environ.get("YOLO_IOU_THRESHOLD", "0.50"))
+    PATH        = os.environ.get("YOLO_WEIGHTS_PATH", "yolov8n.pt")    # Path to a model. yolov8n.pt means download from PyTorch Hub
+    CONF_THRESH = float(os.environ.get("YOLO_CONF_THRESHOLD", "0.10")) # Confidence threshold
 
-    def __init__(self, url: str = None, chunked: bytes = None):
+    def __init__(self, chunked: bytes = None):
+        """Initializes a yolov8 detector with a binary image
+        
+        Arguments:
+            chunked (bytes): A binary image representation
         """
-        Initializes the class with youtube url and output file.
-        :param url: Has to be as youtube URL,on which prediction is made.
-        :param out_file: A valid output file name.
-        """
-        self._URL = url
         self._bytes = chunked
         self.model = self._load_model()
         self.device = self._get_device()
         self.classes = self.model.names
 
     def _get_device(self):
+        """Gets best device for your system
+
+        Returns:
+            device (str): The device to use for YOLO for your system
+        """
         if platform.system().lower() == "darwin":
             return "mps"
         if torch.cuda.is_available():
@@ -37,64 +35,70 @@ class YoloV8ImageObjectDetection:
         return "cpu"
 
     def _get_image_from_chunked(self):
-        """
-        Creates a new video streaming object to extract video frame by frame to make prediction on.
-        :return: opencv2 video capture object, with lowest quality frame available for video.
+        """Loads an openCV image from the raw image bytes passed by
+        the API.
+
+        Returns: 
+            img (numpy.ndarray): opencv2 image object from the raw binary
         """
         arr = np.asarray(bytearray(self._bytes), dtype=np.uint8)
         img = cv2.imdecode(arr, -1)  # 'Load it as it is'
         return img
 
-    def _get_image_from_url(self):
-        """
-        Creates a new video streaming object to extract video frame by frame to make prediction on.
-        :return: opencv2 video capture object, with lowest quality frame available for video.
-        """
-        req = urlopen(self._URL)
-        self._bytes = req.read()
-        return self._get_image_from_chunked()
 
     def _load_model(self):
-        """
-        Loads Yolo8 model from pytorch hub.
-        :return: Trained Pytorch model.
+        """Loads Yolo8 model from pytorch hub or a path on disk
+
+        Returns:
+            model (Model) - Trained Pytorch model
         """
         model = YOLO(YoloV8ImageObjectDetection.PATH)
         return model
 
     def score_frame(self, frame):
-        """
-        Takes a single frame as input, and scores the frame using yolo5 model.
-        :param frame: input frame in numpy/list/tuple format.
-        :return: Labels and Coordinates of objects detected by model in the frame.
+        """Scores a single image with a YoloV8 model
+
+        Arguments:
+            frame (numpy.ndarray): input frame in numpy/list/tuple format.
+
+        Returns:
+            results list(ultralytics.engine.results.Results)): Labels and Coordinates of objects detected by model in the frame.
         """
         self.model.to(self.device)
         frame = [frame]
-        results = self.model(frame, conf=YoloV8ImageObjectDetection.CONF_THRESH, save_conf=True, iou=YoloV8ImageObjectDetection.IOU)
+        results = self.model(
+            frame, 
+            conf=YoloV8ImageObjectDetection.CONF_THRESH, 
+            save_conf=True
+        )
         return results
 
     def class_to_label(self, x):
-        """
-        For a given label value, return corresponding string label.
-        :param x: numeric label
-        :return: corresponding string label
+        """For a given label value, return corresponding string label.
+        Arguments:
+            x (int): numeric label
+
+        Returns:   
+            class (str): corresponding string label
         """
         return self.classes[int(x)]
 
     def plot_boxes(self, results, frame):
-        """
-        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-        :param results: contains labels and coordinates predicted by model on the given frame.
-        :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
+        """Takes a frame and its results as input, 
+        and plots the bounding boxes and label on to the frame.
+
+        Arguments:
+            results (list(ultralytics.engine.results.Results)): contains labels and coordinates predicted by model on the given frame.
+            frame (numpy.ndarray): Frame which has been scored.
+        
+        Returns:
+            frame (numpy.ndarray): Frame with bounding boxes and labels ploted on it.
+            labels (list(str)): The corresponding labels that were found
         """
         for r in results:
             boxes = r.boxes
             labels = []
             for box in boxes:
-                b = box.xyxy[
-                    0
-                ]  # get box coordinates in (top, left, bottom, right) format
                 c = box.cls
                 l = self.model.names[int(c)]
                 labels.append(l)
@@ -102,17 +106,15 @@ class YoloV8ImageObjectDetection:
         return frame, labels
 
     async def __call__(self):
+        """This function is called when class is executed.
+        It analyzes a single image passed to its constructor
+        and returns the annotated image and its labels
+        
+        Returns:
+            frame (numpy.ndarray): Frame with bounding boxes and labels ploted on it.
+            labels (list(str)): The corresponding labels that were found
         """
-        This function is called when class is executed, it runs the loop to read the video frame by frame,
-        and write the output into a new file.
-        :return: void
-        """
-
-        if self._URL:
-            frame = self._get_image_from_url()
-        else:
-            frame = self._get_image_from_chunked()
-
+        frame = self._get_image_from_chunked()
         results = self.score_frame(frame)
         frame, labels = self.plot_boxes(results, frame)
         return frame, set(labels)
